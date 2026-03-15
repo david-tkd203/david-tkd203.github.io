c8111e1c9a8e68afc99c6d4cdc01036b106ae953
David Nanculeo <dnanculeob@uft.edu>
2026-03-15 20:26:05 -0300

    fix css

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -6,6 +6,7 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
+// Force rebuild - 2026-03-15
 function App() {
 
   return (
